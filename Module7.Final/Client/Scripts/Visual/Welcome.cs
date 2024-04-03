56812003ea8509544ce4ab30acd0cbca6e57a465
TMesmeR <mesmerat@gmail.com>
2024-04-03 12:48:29 +0300

    Небольшое дополнение к коду в визуальной части

--- Module7.Final/Client/Scripts/Visual/Welcome.cs
+++ Module7.Final/Client/Scripts/Visual/Welcome.cs
@@ -1,4 +1,6 @@
-﻿using Module7.Final.Server.User;
+﻿using Module7.Final.Server.Common.Scripts;
+using Module7.Final.Server.Order;
+using Module7.Final.Server.User;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -11,7 +13,17 @@ namespace Module7.Final.Client.Scripts.Visual
 /// </summary>
     internal static class Welcome
     {
-        internal static void Print() => Console.WriteLine($"Добро пожаловать {Server.User.User.clientUser.GetUserName()}");
+        internal static void Print()
+        {
+            Console.WriteLine($"Добро пожаловать {ServerUser.clientUser.GetUserName()}");
 
+            if (ServerUser.clientUser.GetBirthDay().Month == ServerTime.GetServerTime().Month
+                && ServerUser.clientUser.GetBirthDay().Day == ServerTime.GetServerTime().Day)
+            {
+                Console.WriteLine($"{ServerUser.clientUser.GetUserName()} видим что вам исполнилось {ServerUser.clientUser.GetAge()}");
+                Console.WriteLine($"Сегодня вы получите скидку в {ServerOrders.extensionOrder.GetDiscount()} процентов");
+            }
+
+        }
     }
 }
--- Module7.Final/Server/Common.Scripts/globalUsing.cs
+++ Module7.Final/Server/Common.Scripts/globalUsing.cs
@@ -1,1 +1,2 @@
 ﻿global using ServerOrders = Module7.Final.Server.Order.Order;
+global using ServerUser = Module7.Final.Server.User.User;
